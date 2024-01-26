11b8c45e8f6b945f3767ed9723fda9c9acc1f84b
Brent Cleary <brent.cleary@gmail.com>
2024-01-25 20:44:50 -0800

    Add project files.

---
+++ Models/Settings/MovieProSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace MoviePro.Models.Settings
+{
+    public class MovieProSettings
+    {
+        public string TmDbApiKey { get; set; }
+
+        public string DefaultBackdropSize { get; set; }
+
+        public string DefaultPosterSize { get; set; }
+
+        public string DefaultYoutubeKey { get; set; }
+
+        public DefaultCollection DefaultCollection { get; set; }
+
+        public DefaultCredentials DefaultCredentials { get; set; }
+
+    }
+
+    public class DefaultCollection
+    {
+        public string Name { get; set; }
+        public string Desciption { get; set; }
+
+    }
+
+    public class DefaultCredentials
+    {
+        public string Role { get; set; }
+        public string Password { get; set; }
+
+    }
+
+}
+
