401588b3f6b53dd36728b28910985f062b19939e
Brent Cleary <brent.cleary@gmail.com>
2024-01-27 07:29:30 -0800

    Database Models and Landing Page View Model Completed

--- Models/Database/Collection.cs
+++ Models/Database/Collection.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public ICollection<MovieCollection> MovieCollection { get; set; } = new HashSet<MovieCollection>();
+
     }
 }
--- Models/Database/Movie.cs
+++ Models/Database/Movie.cs
@@ -42,6 +42,9 @@ namespace MoviePro.Models.Database
         [Display(Name = "Backdrop Image")]
         public IFormFile BackdropFile { get; set; }
 
+        public ICollection<MovieCollection> Collections { get; set; } = new HashSet<MovieCollection>();
+        public ICollection<MovieCast> Cast { get; set; } = new HashSet<MovieCast>();
+        public ICollection<MovieCrew> Crew { get; set; } = new HashSet<MovieCrew>();
 
     }
 }
