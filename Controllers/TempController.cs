f068caeaf2bc9394a578beef8214a31f1710458f
Brent Cleary <brent.cleary@gmail.com>
2024-01-30 20:22:41 -0800

    Temp Controller Scaffolding Put into Movies Controller and Updated

---
+++ Controllers/TempController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MoviePro.Data;
+using MoviePro.Models.Database;
+
+namespace MoviePro.Controllers
+{
+    public class TempController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TempController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Temp
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movie.ToListAsync());
+        }
+
+        // GET: Temp/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+    }
+
+}
